6d7d5ea65155770779396f026b848c3a20073f92
Lathrell <qltpagsuguiron@tip.edu.ph>
2025-01-04 18:40:48 +0800

    feat: implement note fetching and deletion functionality in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,36 @@
+import { useEffect, useState } from "react";
+import api from "../api";
+
 function Home() {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get("/api/notes/")
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/delete/${id}/`)
+      .then((res) => {
+        if (res.status === 204) alert("Note deleted!");
+        else alert("Failed to delete note.");
+      })
+      .catch((error) => alert(error));
+    getNotes();
+  };
   return <div>Home</div>;
 }
 
